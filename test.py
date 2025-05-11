d19b046c3a7154aa881d237a79cc65b98c6da6b5
Marythore <3065719435@qq.com>
2025-05-11 19:15:59 +0800

    test

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 import torch
+
+# 检查 PyTorch 版本
 print(torch.__version__)
+
+# 检查 CUDA 是否可用
 print(torch.cuda.is_available())
+
+# 查看可用的 CUDA 数量
+print(torch.cuda.device_count())
+
+# 查看 CUDA 的版本号
+print(torch.version.cuda)
--- train.py
+++ train.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     # build from YAML and transfer weights
     model = YOLO('yolo11n.yaml').load('./weights/yolo11n.pt')
     # Train the model
-    model.train(data='./VOCData/mydata.yaml', epochs=300, imgsz=640)
+    model.train(data='D:\Projects\FlightInspection-AIDetection\VOCData\mydata.yaml', epochs=100, imgsz=640, save=True, patience=20, optimizer='AdamW',)
